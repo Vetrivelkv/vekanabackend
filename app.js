919cea0af1e3386cf3bca43cba9f970d6610ba12
Vetrivelkv <vetrvelkvk@gmail.com>
2021-05-02 14:51:52 +0530

    heroku2

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ app.use('/products', product);
 
 
 
-const port=process.env.port || '5000'
 
+let port=process.env.PORT || '5000'
 app.listen(port, () => {
-    console.log('Server is up and running on port numner ' + port);
+    console.log('Server is up and running on port numner ' ,port);
 });
